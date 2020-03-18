fc0277a69a558548299c6018502507c4a68adcfe
Thijs Vogels <t.vogels@me.com>
2020-03-18 11:16:05 +0100

    made scaling relative to SVG size

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -4,7 +4,7 @@ figma.ui.onmessage = msg => {
     if (msg.type === 'create-latex-svg') {
         const node = figma.createNodeFromSvg(msg.svg);
         const svg = node.children[0];
-        svg.resize(msg.scale, svg.height * (msg.scale / svg.width));
+        svg.resize(svg.width * msg.scale, svg.height * msg.scale);
         svg.name = msg.source;
         figma.currentPage.appendChild(svg);
         figma.currentPage.selection = [svg];
